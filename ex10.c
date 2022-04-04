7546bf715e50d2f11f5042cff8bb996dc18c9d49
Breno Lopes <brenolcdev@gmail.com>
2022-04-04 18:56:33 -0300

    add ex9

---
+++ ex10.c
@@ -0,0 +1,36 @@
+/**
+ * @file ex10.c
+ * @author Breno Lopes(www.github.com/zTrolly)
+ * @date 2022-04-03
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+/*
+Em uma eleição presidencial existem quatro candidatos. Os votos são informados através de códigos. Os dados utilizados para a contagem dos votos obedecem à seguinte codificação:
+
+1,2,3,4 = voto para os respectivos candidatos; 
+5 = voto nulo;
+6 = voto em branco;
+
+Elabore um algoritmo que leia o código do candidado em um voto. Calcule e escreva:
+  - total de votos para cada candidato;
+  - total de votos nulos;
+  - total de votos em branco.
+Como finalizador do conjunto de votos, tem-se o valor 0.
+*/
+int main() {
+
+int contadorVotos1,
+    contadorVotos2,
+    contadorVotos3,
+    contadorVotos4,
+    contadorVotos5,
+    contadorVotos6;
+
+
+
+
+
+  return 0;
+}
--- ex9.c
+++ ex9.c
@@ -7,9 +7,60 @@
 #include<stdio.h>
 #include<stdlib.h>
 /*
+Um comerciante deseja fazer o levantamento do lucro das mercadorias que ele comercializa. Para isto, mandou digitar uma linha para cada mercadoria com o preço de compra e de venda de cada uma. A última linha contém preço de compra igual a 0. Escreva um programa que:
+a) Determine e escreva quantas mercadorias proporcionaram: 
+
+i) Lucro < 10%
+ii) 10% <= lucro <= 20%
+iii) Lucro > 20%
+
+b) Determine e escreva o valor total de compra e de venda de todas as mercadorias, assim como o lucro total.
 
 */
 int main() {
+  int contadorMenor10 = 0,
+      contadorEntre10e20 = 0,
+      contadorMaior20 = 0;
+
+    float valorCompra,
+          valorVenda,
+          totalCompra = 0,
+          totalVenda = 0;
   
-  return 0;
+  for (;;){
+
+    printf("Digite o valor de compra -> ");
+    scanf("%f", &valorCompra);
+
+    if(valorCompra == 0){break;}
+
+    printf("Digite o valor de venda -> ");
+    scanf("%f", &valorVenda);
+
+    totalCompra += valorCompra;
+    totalVenda  += valorVenda;
+
+    if(valorVenda > valorCompra * 1.2){
+      contadorMaior20++;
+    }else if (valorVenda < valorVenda *1.1){
+      contadorMenor10++;
+    }else{
+      contadorEntre10e20++;
+    }
+  }
+
+  printf("Quantidade de vendas com lucro menor que 10%%-> %d\n", contadorMenor10);
+  printf("Quantidade de vendas com lucro entre 10%% e 20%%-> %d\n", contadorEntre10e20);
+  printf("Quantidade de vendas com lucro entre 20%%-> %d\n", contadorMaior20);
+  printf("Valor total de vendas -> %.2f\n", totalVenda);
+  printf("Valor total de compras -> %.2f\n", totalCompra);
+
+
+  if(totalVenda - totalCompra > 0){
+    printf("\nO lucro total foi de %.2f\n", totalVenda - totalCompra);
+  }else{
+    printf("\nO prejuizo total foi de %.2f\n", totalVenda - totalCompra);
+  }
+
+
 }
